cd13b0a0ece2b2a2334b1fd43c9869f8fe1370f8
Gabriel Malta <ggm@cesar.school>
2026-06-09 10:12:13 -0300

    corrigindo cicloservice.js

--- frontend/src/services/cicloService.js
+++ frontend/src/services/cicloService.js
@@ -1,31 +1,31 @@
 import { requisicao } from './api'
 
 function listarRegistrosCiclo() {
-  return requisicao('/ciclo/')
+  return requisicao('/ciclo/registros/')
 }
 
 function criarRegistroCiclo(dados) {
-  return requisicao('/ciclo/', {
+  return requisicao('/ciclo/registros/', {
     method: 'POST',
     body: JSON.stringify(dados),
   })
 }
 
 function atualizarRegistroCiclo(id, dados) {
-  return requisicao(`/ciclo/${id}/`, {
+  return requisicao(`/ciclo/registros/${id}/`, {
     method: 'PATCH',
     body: JSON.stringify(dados),
   })
 }
 
 function excluirRegistroCiclo(id) {
-  return requisicao(`/ciclo/${id}/`, {
+  return requisicao(`/ciclo/registros/${id}/`, {
     method: 'DELETE',
   })
 }
 
 function obterPrevisoesCiclo() {
-  return requisicao('/ciclo/previsao/')
+  return requisicao('/ciclo/previsoes/')
 }
 
 export {
