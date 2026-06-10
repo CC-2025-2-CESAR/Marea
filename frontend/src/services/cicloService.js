fa6a4a6178b7c76335ff24f34906188e1a77acc1
Gabriel Malta <ggm@cesar.school>
2026-06-10 12:01:30 -0300

    ajuste salvamento de registros

--- frontend/src/services/cicloService.js
+++ frontend/src/services/cicloService.js
@@ -1,35 +1,30 @@
 import { requisicao } from './api'
 
 function listarRegistrosCiclo() {
-  return requisicao('/ciclo/registros/')
+  return requisicao('/ciclo/')
 }
 
 function criarRegistroCiclo(dados) {
-  return requisicao('/ciclo/registros/', {
+  return requisicao('/ciclo/', {
     method: 'POST',
     body: JSON.stringify(dados),
   })
 }
 
 function atualizarRegistroCiclo(id, dados) {
-  return requisicao(`/ciclo/registros/${id}/`, {
+  return requisicao(/ciclo/${id}/, {
     method: 'PATCH',
     body: JSON.stringify(dados),
   })
 }
 
-function excluirRegistroCiclo(id) {
-  return requisicao(`/ciclo/registros/${id}/`, { method: 'DELETE' })
-}
-
 function obterPrevisoesCiclo() {
-  return requisicao('/ciclo/previsoes/')
+  return requisicao('/ciclo/previsao/')
 }
 
 export {
   listarRegistrosCiclo,
   criarRegistroCiclo,
   atualizarRegistroCiclo,
-  excluirRegistroCiclo,
   obterPrevisoesCiclo,
 }
